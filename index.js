8f5b8776f64e1fabaa3408feb591124c5045ed46
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-24 21:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var itemName = ""
-  var cart = {
-    [itemName]: ``;
+  var cart = { [itemName] }
     [itemPrice: ``;]
   };
    var itemPrice = Math.floor(Math.random() / Math.floor(100));
