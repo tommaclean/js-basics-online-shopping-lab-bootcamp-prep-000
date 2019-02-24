a3425c11065221031f0c662c2b83115c44ed2ee4
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-24 22:00:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function addToCart(item) {
     [itemName]: item,
     [itemPrice]: itemPrice
   }
-  return `${itemName} has been added to your cart.`
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
